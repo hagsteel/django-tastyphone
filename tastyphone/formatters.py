bac42f302f24b9d8e72d823bcdbc56fdbb721064
Jonas Hagstedt <hagstedt@gmail.com>
2012-08-16 14:57:02 +0100

    - removed old source

--- tastyphone/formatters.py
+++
@@ -1,8 +0,0 @@
-class Formatters(object):
-    def pascal_case_format(self, name):
-        return name.title().replace('_','')
-
-    def camel_case_format(self, name):
-        name = name.title().replace('_','')
-        name = name[0].lower() + name[1:]
-        return name
--- tastyphone/tests/CreateModelTest.py
+++
@@ -1,7 +0,0 @@
-from django.core import management
-from django.test import TestCase
-
-class CreateModelTest(TestCase):
-
-    def test_create_model(self):
-        management.call_command('create_models')
