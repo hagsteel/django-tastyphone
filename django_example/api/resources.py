911a23a59c7f318129846b43679d874a6f9b70fd
Jonas Hagstedt <hagstedt@gmail.com>
2012-08-16 16:22:30 +0100

    - Pep8 fixes

--- django_example/api/resources.py
+++ django_example/api/resources.py
@@ -2,11 +2,12 @@ from tastypie.authorization import Authorization
 from tastypie.resources import ModelResource
 from django_example.models import Poll, Question, Vote
 
+
 class PollResource(ModelResource):
     class Meta:
         resource_name = 'poll'
         queryset = Poll.objects.all()
-        allowed_methods = ['get','post','put','delete']
+        allowed_methods = ['get', 'post', 'put', 'delete']
         authorization = Authorization()
 
 
@@ -14,7 +15,7 @@ class QuestionResource(ModelResource):
     class Meta:
         resource_name = 'question'
         queryset = Question.objects.all()
-        allowed_methods = ['get','post','put','delete']
+        allowed_methods = ['get', 'post', 'put', 'delete']
         authorization = Authorization()
 
 
@@ -22,6 +23,5 @@ class VoteResource(ModelResource):
     class Meta:
         resource_name = 'vote'
         queryset = Vote.objects.all()
-        allowed_methods = ['get','post','put','delete']
+        allowed_methods = ['get', 'post', 'put', 'delete']
         authorization = Authorization()
-
--- django_example/models.py
+++ django_example/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 
+
 class Poll(models.Model):
     title = models.CharField(max_length=254, null=False)
+
 
 class Question(models.Model):
     question = models.CharField(max_length=254, null=False)
     poll = models.ForeignKey(Poll, related_name='polls')
 
+
 class Vote(models.Model):
     date_created = models.DateTimeField(auto_now=True)
     question = models.ForeignKey(Question, related_name='questions')
-
